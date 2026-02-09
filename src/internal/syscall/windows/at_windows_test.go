5869fa6352fab2e0b51e012bc60c9dbee83a6f5e
Michal Pristas <michal.pristas@gmail.com>
2026-02-09 11:01:16 +0100

    fixed syscall signatures in syscall/windows

--- src/internal/syscall/windows/at_windows_test.go
+++ src/internal/syscall/windows/at_windows_test.go
@@ -142,7 +142,7 @@ func makeFileNotReadable(t *testing.T, name string) {
 		entryForSid(everyoneSID),
 	}
 
-	var oldAcl, newAcl syscall.Handle
+	var oldAcl, newAcl *windows.ACL
 	if err := windows.SetEntriesInAcl(
 		uint32(len(entries)),
 		&entries[0],
@@ -160,7 +160,7 @@ func makeFileNotReadable(t *testing.T, name string) {
 		nil,
 		nil,
 		newAcl,
-		0,
+		nil,
 	); err != nil {
 		t.Fatal(err)
 	}
